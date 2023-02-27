5efeca5aa9856c38a6603f5174455a899bb9ad1f
noyaaljohny <113782227+noyaaljohny@users.noreply.github.com>
2023-02-28 01:27:39 +0530

    Update StudentCardView.java

--- src/StudentCardView.java
+++ src/StudentCardView.java
@@ -28,13 +28,22 @@ public class StudentCardView extends VBox {
         setPadding(new Insets(10));
         setSpacing(10);
         getChildren().addAll(imageView, firstNameLabel, lastNameLabel, studentNumberLabel, favoriteActivitiesLabel);
+
+        // Load CSS styles and apply to layout container and UI components
+        getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        getStyleClass().add("student-card");
+        imageView.getStyleClass().add("student-card-image");
+        firstNameLabel.getStyleClass().add("student-card-name");
+        lastNameLabel.getStyleClass().add("student-card-name");
+        studentNumberLabel.getStyleClass().add("student-card-number");
+        favoriteActivitiesLabel.getStyleClass().add("student-card-activities");
     }
 
     public void update(Student student) {
         // Update UI components with student information
         imageView.setImage(new Image(student.getImageUrl()));
-        firstNameLabel.setText("First Name: " + student.getFirstName());
-        lastNameLabel.setText("Last Name: " + student.getLastName());
+        firstNameLabel.setText(student.getFirstName());
+        lastNameLabel.setText(student.getLastName());
         studentNumberLabel.setText("Student Number: " + student.getStudentNumber());
         favoriteActivitiesLabel.setText("Favorite Activities: " + String.join(", ", student.getFavoriteActivities()));
     }
