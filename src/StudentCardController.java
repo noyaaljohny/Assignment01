8c823fd09b0ff31d540cdda8c90f6c55d1433817
noyaaljohny <113782227+noyaaljohny@users.noreply.github.com>
2023-02-28 00:34:32 +0530

    Update StudentCardController.java

--- src/StudentCardController.java
+++ src/StudentCardController.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class StudentCardController {
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class StudentCardController {
+    private Student model;
+    private StudentCardView view;
+    private Stage stage;
+
+    public StudentCardController(Student model) {
+        this.model = model;
+        view = new StudentCardView();
+        view.update(model);
+
+        // Set up event handling
+        view.setOnMouseClicked(event -> {
+            System.out.println("Clicked on student card for " + model.getFirstName() + " " + model.getLastName());
+        });
+    }
+
+    public void show(Stage stage) {
+        this.stage = stage;
+        stage.setTitle("Student Card");
+        Scene scene = new Scene(view);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
