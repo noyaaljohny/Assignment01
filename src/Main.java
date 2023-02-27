f351626f7e729766dbad3b6fdc17244147f66b21
noyaaljohny <113782227+noyaaljohny@users.noreply.github.com>
2023-02-28 00:32:35 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.util.Arrays;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Create sample student
+        Student student = new Student("John", "Doe", 123456, Arrays.asList("Soccer", "Swimming"), "https://via.placeholder.com/200");
+
+        // Create controller and show GUI
+        StudentCardController controller = new StudentCardController(student);
+        controller.show(primaryStage);
+    }
 }
